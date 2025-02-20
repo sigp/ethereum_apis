1942afab4adada81e9e9518f1edf05a3b42b0996
jmcph4 <jack@jmcph4.dev>
2025-02-20 10:58:31 +1000

    Add the universal bundle

--- searcher-api-types/src/lib.rs
+++ searcher-api-types/src/lib.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 pub mod beaver;
 pub mod flashbots;
 pub mod titan;
@@ -5,3 +7,16 @@ pub mod titan;
 pub use beaver::*;
 pub use flashbots::*;
 pub use titan::*;
+
+/// Universal bundle submission RPC type
+///
+/// This type represents what Lynx accepts from external order flow providers.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum SendBundleRequest {
+    /// Flashbots bundle
+    Flashbots(FlashbotsBundle),
+    /// Beaverbuild bundle
+    Beaver(BeaverBundle),
+    /// Titan Builder bundle
+    Titan(TitanBundle),
+}
