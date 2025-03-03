778a3fb82f533c74f298fe223b518aeacdcec06a
jmcph4 <jack@jmcph4.dev>
2025-03-04 09:45:39 +1000

    Add deserialisation

--- searcher-api-types/src/lib.rs
+++ searcher-api-types/src/lib.rs
@@ -1,6 +1,6 @@
 use alloy_primitives::{BlockNumber, Bytes, TxHash};
 use alloy_rlp::encode;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 pub mod beaver;
 pub mod flashbots;
@@ -13,7 +13,7 @@ pub use titan::*;
 /// Universal bundle submission RPC type
 ///
 /// This type represents what Lynx accepts from external order flow providers.
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum SendBundleRequest {
     /// Flashbots bundle
