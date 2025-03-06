78d790d90b9f68cae0036cc11f446c94945008f6
Pawan Dhananjay <pawandhananjay@gmail.com>
2025-03-06 12:59:11 -0800

    Add tests for builder server endpoints

--- builder-server/src/server.rs
+++ builder-server/src/server.rs
@@ -123,3 +123,287 @@ where
     tracing::info!("Got response from builder, constructing response");
     build_response_with_headers(res, content_type, api_impl.as_ref().fork_name_at_slot(slot))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_trait::async_trait;
+    use axum::{body::Body, http::Request};
+    use builder_api_types::{
+        builder_bid::{BuilderBid, BuilderBidDeneb, SignedBuilderBid},
+        Address, BeaconBlock, BeaconBlockDeneb, Blob, BlobsBundle, EmptyBlock, ExecutionPayload,
+        ExecutionPayloadAndBlobs, ExecutionPayloadDeneb, ForkName, ForkVersionDecode,
+        ForkVersionedResponse, FullPayloadContents, KzgCommitment, KzgProof, MainnetEthSpec,
+        Signature, Uint256, ValidatorRegistrationData,
+    };
+    use ethereum_apis_common::{ErrorResponse, CONSENSUS_VERSION_HEADER};
+    use http::{HeaderValue, Response};
+    use ssz::Encode;
+    use std::{marker::PhantomData, usize};
+    use tower::ServiceExt;
+
+    pub const PREFERENCE_ACCEPT_VALUE: &str =
+        "application/octet-stream;q=1.0,application/json;q=0.9";
+
+    #[derive(Clone)]
+    struct DummyBuilder<E: EthSpec> {
+        _phantom: PhantomData<E>,
+    }
+
+    impl<E: EthSpec> AsRef<DummyBuilder<E>> for DummyBuilder<E> {
+        fn as_ref(&self) -> &DummyBuilder<E> {
+            self
+        }
+    }
+
+    #[async_trait]
+    impl<E: EthSpec> Builder<E> for DummyBuilder<E> {
+        fn fork_name_at_slot(&self, _slot: Slot) -> builder_api_types::ForkName {
+            ForkName::Deneb
+        }
+
+        async fn get_header(
+            &self,
+            _slot: Slot,
+            _parent_hash: ExecutionBlockHash,
+            _pubkey: PublicKeyBytes,
+        ) -> Result<SignedBuilderBid<E>, ErrorResponse> {
+            Ok(SignedBuilderBid {
+                message: BuilderBid::Deneb(BuilderBidDeneb {
+                    value: Uint256::from(42),
+                    pubkey: PublicKeyBytes::empty(),
+                    blob_kzg_commitments: vec![KzgCommitment::empty_for_testing(); 5].into(),
+                    header: Default::default(),
+                }),
+                signature: Signature::empty(),
+            })
+        }
+
+        async fn register_validators(
+            &self,
+            _registrations: Vec<SignedValidatorRegistrationData>,
+        ) -> Result<(), ErrorResponse> {
+            Ok(())
+        }
+
+        async fn submit_blinded_block(
+            &self,
+            _block: SignedBlindedBeaconBlock<E>,
+        ) -> Result<FullPayloadContents<E>, ErrorResponse> {
+            let payload_and_blobs: ExecutionPayloadAndBlobs<E> = ExecutionPayloadAndBlobs {
+                blobs_bundle: BlobsBundle {
+                    commitments: vec![KzgCommitment::empty_for_testing()].into(),
+                    proofs: vec![KzgProof::empty()].into(),
+                    blobs: vec![Blob::<E>::new(vec![42; E::bytes_per_blob()]).unwrap()].into(),
+                },
+                execution_payload: ExecutionPayload::Deneb(ExecutionPayloadDeneb::default()),
+            };
+            let full_payload = FullPayloadContents::PayloadAndBlobs(payload_and_blobs);
+            Ok(full_payload)
+        }
+    }
+
+    async fn send_request_and_assert_response(
+        request: http::request::Request<Body>,
+        expected_status: StatusCode,
+        check_headers: impl AsyncFn(Response<Body>),
+    ) {
+        let app = new(DummyBuilder::<MainnetEthSpec> {
+            _phantom: PhantomData,
+        });
+
+        let response = app.oneshot(request).await.unwrap();
+        // Assert status code
+        assert_eq!(response.status(), expected_status);
+        // Check headers
+        check_headers(response).await;
+    }
+
+    #[tokio::test]
+    async fn test_registration() {
+        let dummy_registration: ValidatorRegistrations<MainnetEthSpec> = VariableList::from(vec![
+            SignedValidatorRegistrationData {
+                message: ValidatorRegistrationData {
+                    fee_recipient: Address::random(),
+                    gas_limit: 100000,
+                    timestamp: 19939149139,
+                    pubkey: PublicKeyBytes::empty(),
+                },
+                signature: Signature::empty()
+            };
+            100
+        ]);
+
+        // Test ssz request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri("/eth/v1/builder/validators")
+                .method("POST")
+                .header(
+                    CONTENT_TYPE,
+                    HeaderValue::from_static("application/octet-stream"),
+                )
+                .body(Body::from(dummy_registration.as_ssz_bytes()))
+                .unwrap(),
+            StatusCode::OK,
+            async |_| {},
+        )
+        .await;
+
+        // Test json request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri("/eth/v1/builder/validators")
+                .method("POST")
+                .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+                .body(Body::from(serde_json::to_vec(&dummy_registration).unwrap()))
+                .unwrap(),
+            StatusCode::OK,
+            async |_| {},
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_get_header() {
+        // Test ssz request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri(format!(
+                    "/eth/v1/builder/header/{}/{}/{}",
+                    Slot::new(42),
+                    "0x379b447308533668e5323f45b7d5232259f508e8d61ff5b945c9b016792cd94c",
+                    "0xafda62054797148859d1f277ad04e8129bc767c10dae0e2d116f03b87fe9c2a36093a93eab75b4b5bfd3fd0d48816396"
+                ))
+                .method("GET")
+                .header(
+                    ACCEPT,
+                    HeaderValue::from_static("application/octet-stream"),
+                ).body(Body::empty())
+                .unwrap(),
+            StatusCode::OK,
+            async |response| {
+                let headers = response.headers();
+                assert_eq!(headers.get(CONTENT_TYPE).unwrap(), HeaderValue::from_str(&ContentType::Ssz.to_string()).unwrap());
+                assert_eq!(headers.get(ethereum_apis_common::CONSENSUS_VERSION_HEADER).unwrap(), HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap());
+            }
+        )
+        .await;
+
+        // Test json request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri(format!(
+                    "/eth/v1/builder/header/{}/{}/{}",
+                    Slot::new(42),
+                    "0x379b447308533668e5323f45b7d5232259f508e8d61ff5b945c9b016792cd94c",
+                    "0xafda62054797148859d1f277ad04e8129bc767c10dae0e2d116f03b87fe9c2a36093a93eab75b4b5bfd3fd0d48816396"
+                ))
+                .method("GET")
+                .header(
+                    ACCEPT,
+                    HeaderValue::from_static("application/json"),
+                ).body(Body::empty())
+                .unwrap(),
+            StatusCode::OK,
+            async |response| {
+                let headers = response.headers();
+                assert_eq!(headers.get(CONTENT_TYPE).unwrap(), HeaderValue::from_str(&ContentType::Json.to_string()).unwrap());
+                assert_eq!(headers.get(ethereum_apis_common::CONSENSUS_VERSION_HEADER).unwrap(), HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap());
+            }
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_submit_blinded_beacon_block() {
+        let spec = MainnetEthSpec::default_spec();
+        let dummy_block = SignedBlindedBeaconBlock::<MainnetEthSpec>::from_block(
+            BeaconBlock::Deneb(BeaconBlockDeneb::empty(&spec)),
+            Signature::empty(),
+        );
+        // Test ssz request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri("/eth/v1/builder/blinded_blocks")
+                .method("POST")
+                .header(
+                    CONTENT_TYPE,
+                    HeaderValue::from_static("application/octet-stream"),
+                )
+                .header(ACCEPT, HeaderValue::from_static(&PREFERENCE_ACCEPT_VALUE))
+                .header(
+                    CONSENSUS_VERSION_HEADER,
+                    HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap(),
+                )
+                .body(Body::from(dummy_block.as_ssz_bytes()))
+                .unwrap(),
+            StatusCode::OK,
+            async |response: Response<Body>| {
+                let headers = response.headers();
+                assert_eq!(
+                    headers.get(CONTENT_TYPE).unwrap(),
+                    HeaderValue::from_str(&ContentType::Ssz.to_string()).unwrap()
+                );
+                assert_eq!(
+                    headers
+                        .get(ethereum_apis_common::CONSENSUS_VERSION_HEADER)
+                        .unwrap(),
+                    HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap()
+                );
+
+                // Get response body as bytes
+                let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+                    .await
+                    .expect("should get bytes response");
+                assert!(
+                    FullPayloadContents::<MainnetEthSpec>::from_ssz_bytes_by_fork(
+                        &body,
+                        ForkName::Deneb
+                    )
+                    .is_ok()
+                );
+            },
+        )
+        .await;
+
+        // Test json request
+        send_request_and_assert_response(
+            Request::builder()
+                .uri("/eth/v1/builder/blinded_blocks")
+                .method("POST")
+                .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+                .header(ACCEPT, HeaderValue::from_static("application/json"))
+                .header(
+                    CONSENSUS_VERSION_HEADER,
+                    HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap(),
+                )
+                .body(Body::from(serde_json::to_vec(&dummy_block).unwrap()))
+                .unwrap(),
+            StatusCode::OK,
+            async |response: Response<Body>| {
+                let headers = response.headers();
+                assert_eq!(
+                    headers.get(CONTENT_TYPE).unwrap(),
+                    HeaderValue::from_str(&ContentType::Json.to_string()).unwrap()
+                );
+                assert_eq!(
+                    headers
+                        .get(ethereum_apis_common::CONSENSUS_VERSION_HEADER)
+                        .unwrap(),
+                    HeaderValue::from_str(&ForkName::Deneb.to_string()).unwrap()
+                );
+
+                // Get response body as bytes
+                let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+                    .await
+                    .expect("should get bytes response");
+                assert!(serde_json::from_slice::<
+                    ForkVersionedResponse<FullPayloadContents::<MainnetEthSpec>>,
+                >(&body)
+                .is_ok());
+            },
+        )
+        .await;
+    }
+}
