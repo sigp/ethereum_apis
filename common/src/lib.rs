7e8c5a96b25d9b14d85af60ac0e7531342c6d098
Eitan Seri-Levi <eserilev@gmail.com>
2025-03-10 19:11:24 -0600

    Implement builder ssz flow (#8)
    * Impl builder ssz flow
    
    * Update cargo
    
    * fix header
    
    * Use lighthouse unstable and re-export only required crates
    
    * add logging
    
    * Debug
    
    * More debug
    
    * Remove all debugs
    
    * Update lighthouse version
    
    * Add ssz support to register_validators
    
    * Update lighthouse deps to add ssz support to SignedValidatorRegistrationData
    
    * Add tests for builder server endpoints
    
    * Remove debugs
    
    ---------
    
    Co-authored-by: Pawan Dhananjay <pawandhananjay@gmail.com>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,23 +1,104 @@
 use axum::{
-    async_trait,
     body::Body,
     extract::{FromRequest, Request},
     response::{IntoResponse, Response},
 };
-use beacon_api_types::ForkVersionDeserialize;
+use beacon_api_types::{
+    fork_versioned_response::EmptyMetadata, ForkName, ForkVersionDecode, ForkVersionDeserialize,
+    ForkVersionedResponse,
+};
 use bytes::Bytes;
 use flate2::read::GzDecoder;
 use http::header::CONTENT_ENCODING;
 use http::{header::CONTENT_TYPE, HeaderValue, StatusCode};
+use mediatype::{names, MediaType, MediaTypeList};
 use serde::{Deserialize, Serialize};
-use std::io::Read;
+use ssz::Encode;
+use std::{fmt, io::Read, str::FromStr};
 use tracing::error;
 
-pub const CONSENSUS_VERSION_HEADER: &'static str = "Eth-Consensus-Version";
+pub const CONSENSUS_VERSION_HEADER: &str = "Eth-Consensus-Version";
 
-pub async fn build_response<T>(
+pub fn build_response_with_headers<T>(
     result: Result<T, ErrorResponse>,
+    content_type: ContentType,
+    fork_name: ForkName,
 ) -> Result<Response<Body>, StatusCode>
+where
+    T: Serialize + Encode + Send + 'static,
+{
+    let response_builder = Response::builder();
+
+    let resp = match result {
+        Ok(body) => {
+            let mut response = response_builder.status(200);
+
+            if let Some(response_headers) = response.headers_mut() {
+                response_headers.insert(
+                    CONSENSUS_VERSION_HEADER,
+                    HeaderValue::from_str(&fork_name.to_string()).map_err(|e| {
+                        error!(error = ?e);
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    })?,
+                );
+
+                response_headers.insert(
+                    CONTENT_TYPE,
+                    HeaderValue::from_str(&content_type.to_string()).map_err(|e| {
+                        error!(error = ?e);
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    })?,
+                );
+            }
+            let body_content = match content_type {
+                ContentType::Json => {
+                    let body = ForkVersionedResponse {
+                        version: Some(fork_name),
+                        metadata: EmptyMetadata {},
+                        data: body,
+                    };
+                    serde_json::to_vec(&body).map_err(|e| {
+                        error!(error = ?e);
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    })?
+                }
+
+                ContentType::Ssz => T::as_ssz_bytes(&body),
+            };
+            response.body(Body::from(body_content)).map_err(|e| {
+                error!(error = ?e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })
+        }
+        Err(body) => {
+            let mut response = response_builder.status(body.code);
+
+            if let Some(response_headers) = response.headers_mut() {
+                response_headers.insert(
+                    CONTENT_TYPE,
+                    HeaderValue::from_str(&content_type.to_string()).map_err(|e| {
+                        error!(error = ?e);
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    })?,
+                );
+            }
+
+            let body_content = serde_json::to_vec(&body).map_err(|e| {
+                error!(error = ?e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+
+            response.body(Body::from(body_content)).map_err(|e| {
+                error!(error = ?e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })
+        }
+    };
+
+    resp
+}
+
+pub fn build_response<T>(result: Result<T, ErrorResponse>) -> Result<Response<Body>, StatusCode>
 where
     T: Serialize + Send + 'static,
 {
@@ -37,17 +118,10 @@ where
                 );
             }
 
-            let body_content = tokio::task::spawn_blocking(move || {
-                serde_json::to_vec(&body).map_err(|e| {
-                    error!(error = ?e);
-                    StatusCode::INTERNAL_SERVER_ERROR
-                })
-            })
-            .await
-            .map_err(|e| {
+            let body_content = serde_json::to_vec(&body).map_err(|e| {
                 error!(error = ?e);
                 StatusCode::INTERNAL_SERVER_ERROR
-            })??;
+            })?;
 
             response.body(Body::from(body_content)).map_err(|e| {
                 error!(error = ?e);
@@ -67,17 +141,10 @@ where
                 );
             }
 
-            let body_content = tokio::task::spawn_blocking(move || {
-                serde_json::to_vec(&body).map_err(|e| {
-                    error!(error = ?e);
-                    StatusCode::INTERNAL_SERVER_ERROR
-                })
-            })
-            .await
-            .map_err(|e| {
+            let body_content = serde_json::to_vec(&body).map_err(|e| {
                 error!(error = ?e);
                 StatusCode::INTERNAL_SERVER_ERROR
-            })??;
+            })?;
 
             response.body(Body::from(body_content)).map_err(|e| {
                 error!(error = ?e);
@@ -93,7 +160,6 @@ where
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Ssz<T>(pub T);
 
-#[async_trait]
 impl<T, S> FromRequest<S> for Ssz<T>
 where
     T: ssz::Decode,
@@ -110,9 +176,51 @@ where
                 let bytes = Bytes::from_request(req, state)
                     .await
                     .map_err(IntoResponse::into_response)?;
-                return Ok(T::from_ssz_bytes(&bytes)
+                return T::from_ssz_bytes(&bytes)
                     .map(Ssz)
-                    .map_err(|_| StatusCode::BAD_REQUEST.into_response())?);
+                    .map_err(|_| StatusCode::BAD_REQUEST.into_response());
+            }
+        }
+
+        Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())
+    }
+}
+
+#[must_use]
+#[derive(Debug, Clone, Copy, Default)]
+pub struct JsonOrSszWithFork<T>(pub T);
+
+impl<T, S> FromRequest<S> for JsonOrSszWithFork<T>
+where
+    T: serde::de::DeserializeOwned + ForkVersionDecode + 'static,
+    S: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
+        let headers = req.headers().clone();
+        let content_type = headers
+            .get(CONTENT_TYPE)
+            .and_then(|value| value.to_str().ok());
+        let fork_name = headers
+            .get(CONSENSUS_VERSION_HEADER)
+            .and_then(|value| ForkName::from_str(value.to_str().unwrap()).ok());
+
+        let bytes = Bytes::from_request(req, _state)
+            .await
+            .map_err(IntoResponse::into_response)?;
+
+        if let Some(content_type) = content_type {
+            if content_type.starts_with(&ContentType::Json.to_string()) {
+                let payload: T = serde_json::from_slice(&bytes)
+                    .map_err(|_| StatusCode::BAD_REQUEST.into_response())?;
+                return Ok(Self(payload));
+            }
+
+            if content_type.starts_with(&ContentType::Ssz.to_string()) {
+                let payload = T::from_ssz_bytes_by_fork(&bytes, fork_name.unwrap())
+                    .map_err(|_| StatusCode::BAD_REQUEST.into_response())?;
+                return Ok(Self(payload));
             }
         }
 
@@ -124,7 +232,6 @@ where
 #[derive(Debug, Clone, Copy, Default)]
 pub struct JsonOrSsz<T>(pub T);
 
-#[async_trait]
 impl<T, S> FromRequest<S> for JsonOrSsz<T>
 where
     T: serde::de::DeserializeOwned + ssz::Decode + 'static,
@@ -164,7 +271,6 @@ where
 #[derive(Debug, Clone, Copy, Default)]
 pub struct JsonOrSszMaybeGzipped<T>(pub T);
 
-#[async_trait]
 impl<T, S> FromRequest<S> for JsonOrSszMaybeGzipped<T>
 where
     T: serde::de::DeserializeOwned + ssz::Decode + 'static,
@@ -215,7 +321,7 @@ where
 }
 
 // Headers
-#[derive(Default, Clone, Copy)]
+#[derive(Default, Clone, Copy, Debug)]
 pub enum ContentType {
     #[default]
     Json,
@@ -288,7 +394,6 @@ pub fn custom_internal_err(message: String) -> ErrorResponse {
 #[derive(Debug, Clone, Copy, Default)]
 pub struct JsonConsensusVersionHeader<T>(pub T);
 
-#[async_trait]
 impl<T, S> FromRequest<S> for JsonConsensusVersionHeader<T>
 where
     T: ForkVersionDeserialize + 'static,
@@ -315,5 +420,80 @@ where
         )
         .map_err(|_| StatusCode::BAD_REQUEST.into_response())?;
         Ok(Self(result))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Accept {
+    Json,
+    Ssz,
+    Any,
+}
+
+impl fmt::Display for Accept {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Accept::Ssz => write!(f, "application/octet-stream"),
+            Accept::Json => write!(f, "application/json"),
+            Accept::Any => write!(f, "*/*"),
+        }
+    }
+}
+
+impl FromStr for Accept {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let media_type_list = MediaTypeList::new(s);
+
+        // [q-factor weighting]: https://datatracker.ietf.org/doc/html/rfc7231#section-5.3.2
+        // find the highest q-factor supported accept type
+        let mut highest_q = 0_u16;
+        let mut accept_type = None;
+
+        const APPLICATION: &str = names::APPLICATION.as_str();
+        const OCTET_STREAM: &str = names::OCTET_STREAM.as_str();
+        const JSON: &str = names::JSON.as_str();
+        const STAR: &str = names::_STAR.as_str();
+        const Q: &str = names::Q.as_str();
+
+        media_type_list.into_iter().for_each(|item| {
+            if let Ok(MediaType {
+                ty,
+                subty,
+                suffix: _,
+                params,
+            }) = item
+            {
+                let q_accept = match (ty.as_str(), subty.as_str()) {
+                    (APPLICATION, OCTET_STREAM) => Some(Accept::Ssz),
+                    (APPLICATION, JSON) => Some(Accept::Json),
+                    (STAR, STAR) => Some(Accept::Any),
+                    _ => None,
+                }
+                .map(|item_accept_type| {
+                    let q_val = params
+                        .iter()
+                        .find_map(|(n, v)| match n.as_str() {
+                            Q => {
+                                Some((v.as_str().parse::<f32>().unwrap_or(0_f32) * 1000_f32) as u16)
+                            }
+                            _ => None,
+                        })
+                        .or(Some(1000_u16));
+
+                    (q_val.unwrap(), item_accept_type)
+                });
+
+                match q_accept {
+                    Some((q, accept)) if q > highest_q => {
+                        highest_q = q;
+                        accept_type = Some(accept);
+                    }
+                    _ => (),
+                }
+            }
+        });
+        accept_type.ok_or_else(|| "accept header is not supported".to_string())
     }
 }
